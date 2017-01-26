879335d5158cd8874465577f0e4c35a600764dff
Kristian Aurlien <kristian@aurlien.net>
2017-01-26 19:50:52 +0100

    f**ing gaussian

--- Project 2/miniproject2/sarsa.py
+++ Project 2/miniproject2/sarsa.py
@@ -3,6 +3,7 @@ import sys
 import pylab as plb
 import numpy as np
 import mountaincar
+import matplotlib.pyplot as plt
 
 import pickle
 
@@ -10,7 +11,7 @@ class SARSAAgent():
     """A not so good agent for the mountain-car task.
     """
 
-    def __init__(self, mountain_car = None, N=20, eta = 0.01, gamma=0.95, tau=0.1, lambda_eligibility=0.7):
+    def __init__(self, mountain_car=None, N=20, eta=0.005, gamma=0.99, tau=0.01, lambda_eligibility=0.95):
         # Gridworld / neural net size
         self.N = N
 
@@ -39,8 +40,8 @@ class SARSAAgent():
         dx_centers = np.linspace(-15, 15, self.N)
 
         # Gaussian width between the centers
-        self.var_x = (150 + 30) / self.N
-        self.var_dx = (15 + 15) / self.N
+        self.var_x = ((150 + 30) / self.N)**2
+        self.var_dx = ((15 + 15) / self.N)**2
 
         # Create grid given the centers
         self.x_grid, self.dx_grid = np.meshgrid(x_centers, dx_centers)
@@ -58,40 +59,34 @@ class SARSAAgent():
         Initialize the Q-values, eligibility trace, position etc.
         """
         # initialize the Q-values, weights and the eligibility trace
-        self.w = 0.01 * np.random.rand(3, self.N**2) + 0.1
-        self._reset_values()
+        self.w = np.zeros((3, self.N ** 2))
+        self._reset_e_values()
 
         # list that contains the times it took the agent to reach the target for all trials
         # serves to track the progress of learning
         self.latency_list = []
 
-    def _reset_values(self):
-        self.Q = 0.01 * np.random.rand(self.N,self.N,3) + 0.1
+    def _reset_e_values(self):
         self.e = np.zeros((3, self.N**2))
-
 
     def _rj_activity(self, state):
         current_x, current_dx = state
 
-        # r_j for all j
-        rj = np.exp(-((self.x_grid - current_x) ** 2) / self.var_x - ((self.dx_grid - current_dx) ** 2) / self.var_dx)
+        x_dist = np.abs(self.x_grid - current_x).flatten()
+        dx_dist = np.abs(self.dx_grid - current_dx).flatten()
 
-        return rj.flatten() # N*N vector
+        rj = np.exp(-(x_dist ** 2)/self.var_x - (dx_dist ** 2) / self.var_dx)
+        return rj # N*N vector
 
     def _Q_activity(self, state):
         rj = self._rj_activity(state)
 
         return np.dot(self.w, rj) # 3 vector
 
-    def _w_update(self):
-        delta = self._TD_error()
-        delta_w = self.eta * np.multiply(delta, self.e)
-        self.w += delta_w
-
     def _e_udpdate(self, state, action):
-        self.e *= self.gamma * self.lambda_eligibility
+        self.e[action, :] += self._rj_activity(state)
+        self.e[:, :] *= self.gamma * self.lambda_eligibility
 
-        self.e[action,:] += self._rj_activity(state)
 
     def _action_probabilities(self, state):
         # Softmax
@@ -147,20 +142,12 @@ class SARSAAgent():
             next_action = agent._next_action(next_state)
             reward = self.mountain_car.R
 
-            Q_sa = agent._Q_activity(state)[action]
-            Q_next_sa = agent._Q_activity(next_state)[next_action]
-
-            td_error = reward - (Q_sa - self.gamma*Q_next_sa)
-
-            self.w += self.eta * td_error * self.e
-            self._e_udpdate(state, action)
-
-            state, action = next_state, next_action
-
             # check for rewards
             if self.mountain_car.R > 0.0:
                 print("\rreward obtained at t = ", self.mountain_car.t)
                 break
+
+
         return self.mountain_car.t
 
 
@@ -184,11 +171,11 @@ class Simulator():
         else:
             self.mountain_car = mountain_car
 
-        self.SARASA = SARSAAgent(mountain_car = mountain_car)
+        self.SARSA = SARSAAgent(mountain_car = mountain_car)
 
 
     def run_episode(self, n_steps=200):
-        """Do a trial without learning, with display.
+        """Do a trial with learning, without display.
 
         Parameters
         ----------
@@ -198,14 +185,15 @@ class Simulator():
         # Initialize
         # make sure the mountain-car is reset
         self.mountain_car.reset()
-        # and so is SARSAAgent Q and e
-        self.SARASA._reset_values()
+        # and so is SARSAAgent eligibility trace
+        self.SARSA._reset_e_values()
 
         # get the initial state
         state = self.mountain_car.state()
 
         # choose an action from the policy:
-        action = self.SARASA._next_action(state)
+        action = self.SARSA._next_action(state)
+
         for n in range(n_steps):
             self.mountain_car.apply_force(action -1)
 
@@ -213,33 +201,46 @@ class Simulator():
             self.mountain_car.simulate_timesteps(100, 0.01)
 
             next_state = self.mountain_car.state()
-            next_action = self.SARASA._next_action(next_state)
+            next_action = self.SARSA._next_action(next_state)
             reward = self.mountain_car.R
 
-            Q_sa = self.SARASA._Q_activity(state)[action]
-            Q_next_sa = self.SARASA._Q_activity(next_state)[next_action]
+            Q_sa = self.SARSA._Q_activity(state)[action]
+            Q_next_sa = self.SARSA._Q_activity(next_state)[next_action]
 
-            td_error = reward - (Q_sa - self.SARASA.gamma*Q_next_sa)
+            self.SARSA._e_udpdate(state, action)
 
-            self.SARASA.w += self.SARASA.eta * td_error * self.SARASA.e
-            self.SARASA._e_udpdate(state, action)
+            td_error = reward - (Q_sa - self.SARSA.gamma * Q_next_sa)
 
-            state, action, next_state, next_action = next_state, next_action, None, None
+            self.SARSA.w += self.SARSA.eta * td_error * self.SARSA.e
+
+            state, action = next_state, next_action
 
             # check if the episode is finished
             if reward > 0.0:
-                print("(%d)" % n, end="")
                 sys.stdout.flush()
                 return self.mountain_car.t
+
 
         # Did not reach the reward
         print("(*)", end="")
         return self.mountain_car.t
 
 if __name__ == "__main__":
-    n_agents = 1
-    n_steps = 10000
-    n_episodes = 500
+
+    agent = SARSAAgent()
+
+
+    #state = (-60,0)
+    #activity = agent._rj_activity(state).reshape((20, 20))
+    #plt.matshow(activity)
+    #plt.show()
+    #input("...")
+
+
+
+    n_agents = 5
+    n_steps = 5000
+    n_episodes = 200
 
     results = []
     agents = [Simulator() for _ in range(n_agents)]
