9dd1fe37214a1c67fe9806404331a6f398d4a0c3
Kristian Aurlien <kristian@aurlien.net>
2017-01-27 10:18:29 +0100

    some refactoring ++

--- Project 2/miniproject2/curve.py
+++ Project 2/miniproject2/curve.py
@@ -1,8 +1,18 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sb
 
-results = pickle._load(open( "results.pkl", "rb" ))[:200]
+results = pickle._load(open( "tau_variations.pkl", "rb" ))
 
-plt.semilogy(np.mean(results, axis=1))
+fig, axs = plt.subplots(4, sharex=True, sharey=True)
+
+fig.set_size_inches(10, 20, forward=True)
+fig.suptitle("Exploring the tau-parameter", fontsize=20)
+
+for i,key in enumerate(results):
+    axs[i].plot(results[key])
+    axs[i].set_title(key)
+
+
 plt.show()
--- Project 2/miniproject2/sarsa.py
+++ Project 2/miniproject2/sarsa.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 import pickle
 
 class SARSAAgent():
-    """A not so good agent for the mountain-car task.
+    """A pretty good agent for the mountain-car task.
     """
 
-    def __init__(self, mountain_car=None, N=20, eta=0.05, gamma=0.99, tau_0=.1, lambda_eligibility=0.95):
-        # Gridworld / neural net size
-        self.N = N
+    def __init__(self, mountain_car=None, size=20, eta=0.05, gamma=0.99, tau=.1, eligibity_trace_decay=0.95, tau_decay=True):
+        # GridWorld / neural net size
+        self.N = size
 
         self.t = 0
 
@@ -32,16 +32,17 @@ class SARSAAgent():
         # the decay factor for the eligibility trace the
         # default is 0., which corresponds to no eligibility
         # trace at all.
-        self.lambda_eligibility = lambda_eligibility
+        self.eligibity_trace_decay = eligibity_trace_decay
 
         # Exploration parameter
-        self.tau_0 = tau_0
+        self.tau = tau
+        self.tau_decay = tau_decay
 
         # Grid centers
         x_centers = np.linspace(-150, 30, self.N)
         dx_centers = np.linspace(-15, 15, self.N)
 
-        # Gaussian width between the centers
+        # Variance for the input function of the centers
         self.var_x = ((150 + 30) / self.N)**2
         self.var_dx = ((15 + 15) / self.N)**2
 
@@ -53,29 +54,22 @@ class SARSAAgent():
         else:
             self.mountain_car = mountain_car
 
-        # initialize the Q-values etc.
-        self._init_values()
-
-    def _tau(self):
-        tau = 0.0005 + self.tau_0 * np.exp(- .0001 * self.t)
-        return tau
-
-    def _init_values(self):
-        """
-        Initialize the Q-values, eligibility trace, position etc.
-        """
-        # initialize the Q-values, weights and the eligibility trace
+        # initialize the weights and eligibility traces
         self.w = np.zeros((3, self.N ** 2))
         self._reset_e_values()
 
-        # list that contains the times it took the agent to reach the target for all trials
-        # serves to track the progress of learning
-        self.latency_list = []
+    def _tau(self):
+        if self.tau_decay:
+            tau = 0.0005 + self.tau * np.exp(- .0001 * self.t)
+        else:
+            tau = self.tau
+
+        return tau
 
     def _reset_e_values(self):
-        self.e = np.zeros((3, self.N**2))
+        self.traces = np.zeros((3, self.N ** 2))
 
-    def _rj_activity(self, state):
+    def _input_activity(self, state):
         current_x, current_dx = state
 
         x_dist = np.abs(self.x_grid - current_x).flatten()
@@ -84,19 +78,18 @@ class SARSAAgent():
         rj = np.exp(-(x_dist ** 2)/self.var_x - (dx_dist ** 2) / self.var_dx)
         return rj # N*N vector
 
-    def _Q_activity(self, state):
-        rj = self._rj_activity(state)
+    def _output_activity(self, state):
+        rj = self._input_activity(state)
 
         return np.dot(self.w, rj) # 3 vector
 
-    def _e_udpdate(self, state, action):
-        self.e[action, :] += self._rj_activity(state)
-        self.e[:, :] *= self.gamma * self.lambda_eligibility
-
+    def _update_traces(self, state, action):
+        self.traces[action, :] += self._input_activity(state)
+        self.traces[:, :] *= self.gamma * self.eligibity_trace_decay
 
     def _action_probabilities(self, state):
         # Softmax
-        x = self._Q_activity(state) / self._tau()
+        x = self._output_activity(state) / self._tau()
         e_x = np.exp(x - np.max(x))
         probabilities = e_x / e_x.sum()
         return probabilities
@@ -121,11 +114,6 @@ class SARSAAgent():
         mv.create_figure(n_steps, n_steps)
         plb.draw()
 
-        # get our SARSA agent
-        if agent is None:
-            self.agent = SARSAAgent()
-        else:
-            self.agent = agent
 
         # make sure the mountain-car is reset
         self.mountain_car.reset()
@@ -137,136 +125,121 @@ class SARSAAgent():
         action = self._next_action(state)
 
         for n in range(n_steps):
-            #print('\rtime =', self.mountain_car.t)
-            #sys.stdout.flush()
-
-            # choose action from the policy
+            # Apply action
             self.mountain_car.apply_force(action - 1)
-            # simulate the timestep
+
+            # Simulate the time step
             self.mountain_car.simulate_timesteps(100, 0.01)
 
-            next_state = self.mountain_car.state()
-            next_action = agent._next_action(next_state)
+            # Observe the reward
             reward = self.mountain_car.R
 
             # check for rewards
-            if self.mountain_car.R > 0.0:
+            if reward > 0.0:
                 print("\rreward obtained at t = ", self.mountain_car.t)
                 break
 
+            # Get the next action
+            state = self.mountain_car.state()
+            action = agent._next_action(state)
 
         return self.mountain_car.t
 
+    def learn(self, n_episodes, max_steps):
 
+        step_history = []
+        for episode in range(n_episodes):
+            self.mountain_car.reset()
+            self._reset_e_values()
 
-    def learn(self):
-        return
+            state = self.mountain_car.state()
+            action = self._next_action(state)
 
-#
-# if __name__ == "__main__":
-#     s = SARSAAgent()
-#     s.learn()
-#     plb.show()
+            for step in range(max_steps):
 
+                self.mountain_car.apply_force(action - 1)
 
-class Simulator():
+                # simulate the timestep
+                self.mountain_car.simulate_timesteps(100, 0.01)
+                self.t += 1  # self.mountain_car.t
 
-    def __init__(self, mountain_car=None):
+                next_state = self.mountain_car.state()
+                next_action = self._next_action(next_state)
+                reward = self.mountain_car.R
 
-        if mountain_car is None:
-            self.mountain_car = mountaincar.MountainCar()
-        else:
-            self.mountain_car = mountain_car
+                Q_sa = self._output_activity(state)[action]
+                Q_next_sa = self._output_activity(next_state)[next_action]
 
-        self.SARSA = SARSAAgent(mountain_car = mountain_car)
+                self._update_traces(state, action)
 
+                td_error = reward - (Q_sa - self.gamma * Q_next_sa)
 
-    def run_episode(self, n_steps=200):
-        """Do a trial with learning, without display.
+                self.w += self.eta * td_error * self.traces
 
-        Parameters
-        ----------
-        n_steps -- number of steps to simulate for
-        """
+                state, action = next_state, next_action
 
-        # Initialize
-        # make sure the mountain-car is reset
-        self.mountain_car.reset()
-        # and so is SARSAAgent eligibility trace
-        self.SARSA._reset_e_values()
+                # check if the episode is finished
+                if reward > 0.0:
+                    break
 
-        # get the initial state
-        state = self.mountain_car.state()
+            step_history.append(self.mountain_car.t)
+            print("Episode %d: %d" % (episode, self.mountain_car.t))
+        return step_history
 
-        # choose an action from the policy:
-        action = self.SARSA._next_action(state)
-
-        for n in range(n_steps):
-            self.mountain_car.apply_force(action -1)
-
-            # simulate the timestep
-            self.mountain_car.simulate_timesteps(100, 0.01)
-            self.SARSA.t += 1 #self.mountain_car.t
-
-            next_state = self.mountain_car.state()
-            next_action = self.SARSA._next_action(next_state)
-            reward = self.mountain_car.R
-
-            Q_sa = self.SARSA._Q_activity(state)[action]
-            Q_next_sa = self.SARSA._Q_activity(next_state)[next_action]
-
-            self.SARSA._e_udpdate(state, action)
-
-            td_error = reward - (Q_sa - self.SARSA.gamma * Q_next_sa)
-
-            self.SARSA.w += self.SARSA.eta * td_error * self.SARSA.e
-
-            state, action = next_state, next_action
-
-            # check if the episode is finished
-            if reward > 0.0:
-                sys.stdout.flush()
-                return self.mountain_car.t
-
-
-        # Did not reach the reward
-        print("(*)", end="")
-        return self.mountain_car.t
 
 if __name__ == "__main__":
-    '''
-    agent = SARSAAgent()
-    print(agent.var_x, agent.var_dx)
+    n_agents = 5
+    max_steps = 5000
+    n_episodes = 100
+
+    # Explore tau-values:
+    agents = {
+        ('tau=0, no decay', SARSAAgent(tau=0, tau_decay=False)),
+        ('tau=1, no decay', SARSAAgent(tau=1, tau_decay=False)),
+        ('tau=inf, no decay', SARSAAgent(tau=np.inf, tau_decay=False)),
+        ('tau=1, decay', SARSAAgent(tau=1, tau_decay=True))
+    }
+
+    results = {}
+    for name, agent in agents:
+        result = agent.learn(n_episodes, max_steps)
+        results[name] = result
+        print(name, results)
+
+    pickle.dump(results, open("tau_variations.pkl", "wb"))
+
+    # Explore eligibility_trace_decay-values:
+    agents = {
+        ('lambda = 0.95', SARSAAgent(tau=1, tau_decay=True, eligibity_trace_decay=0.95)),
+        ('lambda = 0.5', SARSAAgent(tau=1, tau_decay=True, eligibity_trace_decay=0.5)),
+        ('lambda = 0.95', SARSAAgent(tau=1, tau_decay=True, eligibity_trace_decay=0.0))
+    }
+
+    results = {}
+    for name, agent in agents:
+        result = agent.learn(n_episodes, max_steps)
+        results[name] = result
+        print(name, results)
+
+    pickle.dump(results, open("tau_variations.pkl", "wb"))
 
 
-    state = (0,0)
-    activity = agent._rj_activity(state).reshape((20, 20))
-
-    #print(activity)
-    plt.matshow(activity)
-    plt.show()
-    '''
 
 
-
-    n_agents = 10
-    n_steps = 5000
-    n_episodes = 200
-
-    results = []
-    agents = [Simulator() for _ in range(n_agents)]
-
-    print("Starting simulation (%d agents, %d episodes):" % (n_agents, n_episodes))
-    for i in range(n_episodes):
-        print("Episode %d:" % i, end=' ')
-        round = []
-        for a in agents:
-            res = a.run_episode(n_steps)
-            round.append(res)
-
-        results.append(round)
-        print('\n\tAverage completion time:', np.mean(round))
-
-    pickle.dump(results, open("results.pkl", "wb"))
-    print(results)
-    input("Press Enter to continue...")
+    # results = []
+    # agents = [SARSAAgent() for _ in range(n_agents)]
+    #
+    # print("Starting simulation (%d agents, %d episodes):" % (n_agents, n_episodes))
+    # for i in range(n_episodes):
+    #     print("Episode %d:" % i, end=' ')
+    #     round = []
+    #     for a in agents:
+    #         res = a.run_episode(max_steps)
+    #         round.append(res)
+    #
+    #     results.append(round)
+    #     print('\n\tAverage completion time:', np.mean(round))
+    #
+    # pickle.dump(results, open("results.pkl", "wb"))
+    # print(results)
+    # input("Press Enter to continue...")
