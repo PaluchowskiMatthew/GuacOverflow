474250244f78a8594cc6d6f8f32b396a3dbbdf92
Kristian Aurlien <kristian@aurlien.net>
2017-01-27 12:52:43 +0100

    merge

--- Project 2/miniproject2/curve.py
+++ Project 2/miniproject2/curve.py
@@ -6,35 +6,32 @@ import seaborn as sb
 
 def tau_plots():
     results = pickle._load(open( "tau_variations.pkl", "rb" ))
-    fig, axs = plt.subplots(4, sharex=True, sharey=True)
+    fig_tau, axs_tau = plt.subplots(4, sharex=True, sharey=True)
 
-    fig.set_size_inches(10, 20, forward=True)
-    fig.suptitle("Exploring the tau-parameter", fontsize=20)
+    fig_tau.set_size_inches(10, 20, forward=True)
+    fig_tau.suptitle("Exploring the tau-parameter", fontsize=20)
 
-    for i,key in enumerate(results):
-        axs[i].plot(results[key])
-        axs[i].set_title(key)
+    for i,key in enumerate(sorted(results)):
+        axs_tau[i].plot(results[key])
+        axs_tau[i].set_title(key)
 
-
-    plt.show()
-    plt.savefig("tau_variations.png")
+    fig_tau.savefig("tau_variations.png")
 
 def lambda_plots():
     results = pickle._load(open("lambda_variations.pkl", "rb"))
 
-    fig, axs = plt.subplots(3, sharex=True, sharey=True)
+    fig_lamb, axs_lamb = plt.subplots(3, sharex=True, sharey=True)
 
-    fig.set_size_inches(10, 20, forward=True)
-    fig.suptitle("Exploring the lambda-parameter", fontsize=20)
+    fig_lamb.set_size_inches(10, 20, forward=True)
+    fig_lamb.suptitle("Exploring the lambda-parameter", fontsize=20)
 
-    for i, key in enumerate(results):
-        axs[i].plot(results[key])
-        axs[i].set_title(key)
+    for i, key in enumerate(sorted(results)):
+        axs_lamb[i].plot(results[key])
+        axs_lamb[i].set_title(key)
 
-    plt.show()
-    plt.savefig("lambda_variations.png")
+    fig_lamb.savefig("lambda_variations.png")
 
 
 if __name__ == "__main__":
-    # tau_plots()
+    tau_plots()
     lambda_plots()
